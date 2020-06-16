f5c8611a099f3d713ee204351123160b426a14aa
Molqus <coomst1213@gmail.com>
2020-06-16 23:14:50 +0900

    get all ranked beatmaps

--- osu/data.py
+++ osu/data.py
@@ -1,6 +1,7 @@
-import datetime
+from datetime import datetime, timedelta
 import json
 import os
+import time
 
 from beatmap import osuAPI
 from db_utils import Database, Table
@@ -22,25 +23,43 @@ def getAllBeatmapData():
     osu_api = osuAPI()
     since = '2000-01-01'
     prev_res = set()
-    res = osu_api.get_beatmaps(since=since)
-    # print(type(res))
+    json_name = '../data/beatmaps.json'
 
-    # 重複削除
-    res_duplicated = {r['beatmap_id'] for r in res} & prev_res
-    res = [r for r in res if r['beatmap_id'] not in res_duplicated]
-    # print(res)
+    while 1:
+        res = osu_api.get_beatmaps(since=since)
+        end_flag = 0
+        if len(res) < 500:
+            end_flag = 1
+        # print(type(res))
 
-    json_name = 'test.json'
-    if os.path.isfile(json_name):
-        with open(json_name, 'ab+') as f:
-            f.seek(-1, 2)
-            f.truncate()
-            f.write(' ,'.encode())
-            f.write(json.dumps(res).encode())
-            f.write(']'.encode())
-    else:
-        with open(json_name, 'w') as f:
-            json.dump(res, f)
+        # 重複削除
+        res_duplicated = {r['beatmap_id'] for r in res} & prev_res
+        res = [r for r in res if r['beatmap_id'] not in res_duplicated]
+        # print(res)
+
+        if os.path.isfile(json_name):
+            with open(json_name, 'ab+') as f:
+                f.seek(-1, 2)
+                f.truncate()
+                for r in res:
+                    f.write(' ,'.encode())
+                    f.write(json.dumps(r).encode())
+                f.write(']'.encode())
+        else:
+            with open(json_name, 'w') as f:
+                json.dump(res, f)
+
+        since = str(datetime.strptime(res[-1]['approved_date'],
+                                      '%Y-%m-%d %H:%M:%S') - timedelta(seconds=1))
+
+        if end_flag:
+            print(f'reached latest ranked beatmap. since: {since}')
+            break
+
+        prev_res = {r['beatmap_id'] for r in res}
+        print(since)
+        print(res[0]['beatmap_id'], res[-1]['beatmap_id'], len(set(prev_res)))
+        time.sleep(10)
 
 
 if __name__ == "__main__":
