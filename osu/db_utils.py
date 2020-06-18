230c1be97e7655f1f2b8135d609d8f203cc5b0f4
Molqus <coomst1213@gmail.com>
2020-06-19 01:29:36 +0900

    fix bug on db_utils and handling error

--- osu/db_utils.py
+++ osu/db_utils.py
@@ -8,13 +8,16 @@ class Database():
         self.table = table
 
     def connect(self):
-        self.connect = sqlite3.conenct(self.db_name)
+        self.connect = sqlite3.connect(self.db_name)
 
     def close(self):
         self.connect.close()
 
     def create_table(self):
-        self.connect.execute(self.table.create())
+        try:
+            self.connect.execute(self.table.create())
+        except sqlite3.Error as e:
+            print(e)
 
 
 class Table():
@@ -28,12 +31,12 @@ class Table():
 
     def create(self):
         column_str = ''
-        for k, v in self.columns:
+        for k, v in self.columns.items():
             if not column_str:
                 column_str += f'{k} {v}'
             else:
                 column_str += f', {k} {v}'
-        syntax = f'create table {self.table_name}({column_str})'
+        syntax = f'create table if not exists {self.table_name}({column_str})'
         return syntax
 
     def insert(self):
