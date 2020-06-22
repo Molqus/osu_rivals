72ab1421225f60c4981583c7d6bd65c9319a5ef8
Molqus <coomst1213@gmail.com>
2020-06-22 16:49:53 +0900

    refactoring db classes

--- osu/db_utils.py
+++ osu/db_utils.py
@@ -14,15 +14,15 @@ class Database():
     def close(self):
         self.connect.close()
 
-    def create_table(self):
+    def create_table(self, columns):
         try:
-            self.cursor.execute(self.table.create())
+            self.cursor.execute(self.table.create(columns=columns))
         except sqlite3.Error as e:
             print(e)
 
-    def insert_table(self, data):
+    def insert_table(self, columns, data):
         try:
-            self.cursor.executemany(self.table.insert(), data)
+            self.cursor.executemany(self.table.insert(columns=columns), data)
             self.connect.commit()
         except sqlite3.Error as e:
             print(e)
@@ -37,16 +37,15 @@ class Database():
 
 class Table():
 
-    def __init__(self, table_name: str, columns: dict, *args, **kwargs) -> None:
+    def __init__(self, table_name: str, *args, **kwargs) -> None:
         self.table_name = table_name
-        self.columns = columns
 
     def name(self) -> str:
         return self.table_name
 
-    def create(self):
+    def create(self, columns):
         column_str = ''
-        for k, v in self.columns.items():
+        for k, v in columns.items():
             if not column_str:
                 column_str += f'{k} {v}'
             else:
@@ -54,8 +53,8 @@ class Table():
         syntax = f'create table if not exists {self.table_name}({column_str})'
         return syntax
 
-    def insert(self):
-        placeholder = '?' + ', ?' * (len(self.columns) - 1)
+    def insert(self, columns):
+        placeholder = '?' + ', ?' * (len(columns) - 1)
         syntax = f'insert into {self.table_name} values ({placeholder})'
         return syntax
 
